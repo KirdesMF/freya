59953dba9787cd7197b0ba38a7828be223c7bac4
KirdesMF <cedgourville@gmail.com>
2023-09-27 00:26:36 +0200

    add content fr

---
+++ src/components/SelectLanguage.tsx
@@ -0,0 +1,58 @@
+import * as Select from "@radix-ui/react-select";
+import { ChevronDownIcon } from "./icons/ChevronDownIcon.tsx";
+import { TranslateIcon } from "./icons/TranslateIcon.tsx";
+import { CheckIcon } from "./icons/CheckIcon.tsx";
+
+const LANGUAGES = [
+  { code: "fr", name: "Français" },
+  { code: "en", name: "English" },
+  { code: "nl", name: "Nederlands" },
+];
+
+const defaultLang = "fr" as const;
+const currentLang = LANGUAGES.find((lang) => lang.code === defaultLang)?.name;
+
+export function SelectLanguage() {
+  function onChange(lang: string) {
+    const [_leadingSlash, _oldLang, ...rest] = window.location.pathname.split("/");
+    const slug = rest.join("/");
+    window.location.pathname = `/${lang}/${slug}`;
+  }
+
+  return (
+    <Select.Root value={defaultLang} onValueChange={onChange}>
+      <Select.Trigger className="flex gap-x-2 items-center justify-center border px-1 py-1 rounded text-sm outline-none">
+        <Select.Icon>
+          <TranslateIcon className="w-[18px] h-[18px]" />
+        </Select.Icon>
+        <Select.Value></Select.Value>
+        <Select.Icon>
+          <ChevronDownIcon className="w-[18px] h-[18px]" />
+        </Select.Icon>
+      </Select.Trigger>
+
+      <Select.Portal>
+        <Select.Content
+          className="border rounded w-[var(--radix-select-trigger-width)] bg-bluegray-1"
+          position="popper"
+        >
+          <Select.Viewport className="p-5px">
+            {LANGUAGES.map((lang) => (
+              <Select.Item
+                key={lang.code}
+                value={lang.code}
+                className="relative gap-x-2 text-xs py-1 pie-10px pis-18px select-none rounded-sm data-[highlighted]:outline-none data-[highlighted]:bg-bluegray-2/50"
+              >
+                <Select.ItemIndicator className="absolute left-0 top-0 bottom-0 flex items-center justify-center w-16px">
+                  <CheckIcon className="w-[12px] h-[12px]" />
+                </Select.ItemIndicator>
+
+                <Select.ItemText>{lang.name}</Select.ItemText>
+              </Select.Item>
+            ))}
+          </Select.Viewport>
+        </Select.Content>
+      </Select.Portal>
+    </Select.Root>
+  );
+}
--- src/components/SwitchColorScheme.tsx
+++ src/components/SwitchColorScheme.tsx
@@ -1,9 +1,26 @@
-import * as Switch from "@radix-ui/react-switch";
+import { SunIcon } from "./icons/SunIcon";
+import * as Tooltip from "@radix-ui/react-tooltip";
 
 export function SwitchColorScheme() {
   return (
-    <Switch.Root className="w-[38px] h-[22px] border border-gray-5 rounded-full">
-      <Switch.Thumb className="rounded-full w-[20px] h-[20px] bg-black border border-white block will-change-transform transition-transform duration-100 data-[state=checked]:translate-x-[16px]" />
-    </Switch.Root>
+    <Tooltip.Provider delayDuration={500}>
+      <Tooltip.Root>
+        <Tooltip.Trigger asChild>
+          <button type="button">
+            <SunIcon className="w-[18px] h-[18px]" />
+          </button>
+        </Tooltip.Trigger>
+
+        <Tooltip.Portal>
+          <Tooltip.Content
+            sideOffset={3}
+            className="bg-bluegray-1 rounded px-2 py-1 leading-none shadow"
+          >
+            <span className="font-400 text-xs">Toggle theme</span>
+            <Tooltip.Arrow className="fill-bluegray-1" />
+          </Tooltip.Content>
+        </Tooltip.Portal>
+      </Tooltip.Root>
+    </Tooltip.Provider>
   );
 }
