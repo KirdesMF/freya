f5a8c736d4e597ea10c071fb793dd399fa871d19
KirdesMF <cedgourville@gmail.com>
2023-11-05 11:17:15 +0100

    add sitemap and robots.txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,10 @@ import UnoCSS from "unocss/astro";
 
 import react from "@astrojs/react";
 import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [UnoCSS({ injectReset: true }), react(), mdx()],
+  site: "https://brainslate.fr",
+  integrations: [UnoCSS({ injectReset: true }), react(), mdx(), sitemap()],
 });
